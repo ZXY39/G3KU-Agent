13ce59a38abeaf4f38e78b6297281e0826da408b
ZXY39 <1045273950@qq.com>
2026-03-14 11:35:49 +0800

    3.11

--- g3ku/web/frontend/src/components/tasks/TaskStatusChip.tsx
+++
@@ -1,6 +0,0 @@
-﻿import { statusLabel, statusTone } from '@/lib/utils/status'
-import type { TaskStatus } from '@/lib/types/task'
-
-export function TaskStatusChip(props: { status: TaskStatus }) {
-  return <span className={`status-chip ${statusTone(props.status)}`}>{statusLabel(props.status)}</span>
-}
